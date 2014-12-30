1bc8b9928e0b1e1f80637f7b772c200f3a903c9c
bound1ess <bound1ess.dev@gmail.com>
2014-12-30 19:35:17 +0600

    Small review and cleanup.

--- app/app.rb
+++ app/app.rb
@@ -1,10 +1,12 @@
+require_relative "env"
+
 require "sinatra"
 require "dotka"
-require_relative "env"
 
 class DotkaDemoApp < Sinatra::Base
 
 	enable :static
+
 	set :public_folder, __dir__ + "/../public"
 
 	get "/" do
@@ -16,18 +18,23 @@ class DotkaDemoApp < Sinatra::Base
 	end
 
 	get "/player/:account_id" do
+
 		dotka = Dotka.new
 		dotka.set_api_key $API_KEY
-		matches = dotka.matches(id = params[:account_id], {
-			"matches_requested" => 10
-		})
+
+		matches = dotka.matches(id = params[:account_id], {"matches_requested" => 10})
 		matches.each { |match| match.load_info dotka }
+
 		erb :player, :layout => true, :locals => {:matches => matches, :account_id => id}
+
 	end
 
 	get "/match/:id" do
+
 		(dotka = Dotka.new).set_api_key $API_KEY
+
 		erb :match, :layout => true, :locals => {:match => dotka.match(params[:id])}
+
 	end
 
 end
